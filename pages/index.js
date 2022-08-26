24ffe66555276cf95ea9d6b7460c8b8ef4a9b079
Mac <mac@MacdeMacBook-Air.local>
2022-08-26 22:06:17 +0800

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-import { ConnectWallet, useAddress, useToken, useTokenBalance, useMintToken } from '@thirdweb-dev/react';
+import { ConnectWallet, useAddress, useNFTDrop, useClaimNFT} from '@thirdweb-dev/react';
 
 export default function Home() {
   const address = useAddress();
-  const token = useToken("0x68DF6C1465458c6d36260F7F495Dd33eE61B097f");
-  const { data: tokenBalance } = useTokenBalance(token, address);
-  const { mutate: mintToken} = useMintToken(token);
+  const nftDrop = useNFTDrop("0x5090c514a72674Fe723B34E6109c7c6Bb7f0A161");
+  const { mutate: claimNft} = useClaimNFT(nftDrop);
  
   return (
     <>
       <ConnectWallet accentColor="#b52097"/>
-      <p>Your Address: { address } </p>
-      <p>Your Balance: { tokenBalance?.displayValue } { tokenBalance?.symbol} </p>
-
-      <button onClick={() => mintToken({
-         amount: 1,
+      <button onClick={() => claimNft({
+         quantity: 1,
          to: address, 
-      })}>Mint Token</button>
+      })}>Mint NFT</button>
     </>
   );
 }
